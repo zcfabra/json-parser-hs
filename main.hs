c30ad9bbc4f3d738e7b56d3550c6cde39378e257
zcfabra <christianwgnam@gmail.com>
2024-06-30 12:39:28 -0400

    flipped either to be idiomatic

--- main.hs
+++ main.hs
@@ -10,18 +10,18 @@ data Json =
     | JsonBool Bool 
     deriving (Show, Eq)
 
-parseBool remaining boolVal = Left (JsonBool boolVal, remaining)
-parseNull t = Left (JsonNull, t)
+parseBool remaining boolVal = Right (JsonBool boolVal, remaining)
+parseNull t = Right (JsonNull, t)
 parseStr s =
     let 
         aux str acc = 
             case str of
-            ('"': t) -> Left (acc, t)
+            ('"': t) -> Right (acc, t)
             (x:t) ->  aux t (x : acc)
-            [] -> Right "No Closing"
+            [] -> Left "No Closing"
     in case aux s [] of
-        Left (res, rem) -> Left (JsonString $ reverse res, rem)
-        Right e -> Right e
+        Right (res, rem) -> Right (JsonString $ reverse res, rem)
+        Left e -> Left e
 
 
 stringToFloat str = read str :: Float
@@ -33,53 +33,54 @@ parseNum c =
             (h : t) | isDigit h -> aux t (h:acc) isPastDecimal
             (h : t) | h == '.' -> 
                 if isPastDecimal
-                then Right "Cannot have more than one decimal in a float"
+                then Left "Cannot have more than one decimal in a float"
                 else aux t (h : acc) True 
-            (h : _) | h == ',' || h == '}' || h == ']' -> Left (acc, c')
-            [] -> Left (acc, [])
-            _ -> Right "Invalid Number"
+            (h : _) | h == ',' || h == '}' || h == ']' -> Right (acc, c')
+            [] -> Right (acc, [])
+            _ -> Left "Invalid Number"
     in
         case aux c [] False of
-        Left (res, rem) -> 
-            Left (JsonNumber $ (stringToFloat . reverse) res, rem)
-        Right e -> Right e
+        Right (res, rem) -> 
+            Right (JsonNumber $ (stringToFloat . reverse) res, rem)
+        Left e -> Left e
 
 parseArr c =
     let 
         aux c acc =
             case c of
-                (']':t) -> Left (acc, t)
+                (']':t) -> Right (acc, t)
                 x -> case parse x of
-                    Left (res, rem)-> 
+                    Right (res, rem)-> 
                         case rem of 
                             (',':t) -> aux t (res : acc) 
-                            (']':t) -> Left (res : acc, t)
-                    Right e -> Right e
+                            (']':t) -> Right (res : acc, t)
+                    Left e -> Left e
         in case aux c [] of
-        Left (res, rem) -> Left(JsonArray $ reverse res, rem)
-        Right e -> Right e
+        Right (res, rem) -> Right(JsonArray $ reverse res, rem)
+        Left e -> Left e
 parseObject o = 
     let 
         aux c acc = 
             case c of
-            [] -> Right "Unterminated Object"
-            '}' : t -> Left (acc, t)
+            [] -> Left "Unterminated Object"
+            '}' : t -> Right (acc, t)
             x -> case parse x of
-                Left (JsonString k, rem) -> 
+                Right (JsonString k, rem) -> 
                     case rem of 
                         (':': t) -> 
                             case parse t of 
-                                Left (v, ',':'}':t) -> Right "Cannot end object with `,`"
-                                Left (v, ',':t) -> aux t ((k,v) :acc)
-                                Left (v, '}':t) -> Left ((k,v):acc, t)
-                                Left (v, _) -> Right "Invalid Object Formatting"
-                Right e -> Right e
-                _ -> Right "Need a string as key to Json Object"
+                                Right (v, ',':'}':t) -> Left "Cannot end object with `,`"
+                                Right (v, ',':t) -> aux t ((k,v) :acc)
+                                Right (v, '}':t) -> Right ((k,v):acc, t)
+                                Right (v, _) -> Left "Invalid Object Formatting"
+                                Left e -> Left e
+                Left e -> Left e
+                _ -> Left "Need a string as key to Json Object"
     in case aux o [] of
-        Left (res, rem) -> Left (JsonObject (reverse res), rem)
-        Right e -> Right e
+        Right (res, rem) -> Right (JsonObject (reverse res), rem)
+        Left e -> Left e
 
-parse :: [Char] -> Either (Json, [Char]) String
+parse :: [Char] -> Either String (Json, [Char]) 
 parse c = 
     case c of 
         ('"' : t) -> parseStr t
@@ -89,12 +90,12 @@ parse c =
         ('t':'r':'u':'e' : t) -> parseBool t True
         ('f':'a':'l':'s':'e' : t) -> parseBool t False 
         (h : _) | isDigit h || h == '.' -> parseNum c
-        c -> Right ("Ouch " ++ show c)
+        c -> Left ("Ouch " ++ show c)
 
 
 main :: IO ()
 main = do
     let parsed = parse $ filter (/= ' ') "{\"elements\":[90.0, \"hi\"], \"thing\": null, \"isHappy\": false}"
         in case parsed of
-            Left (res, rem) -> print res
-            Right e -> print e 
+            Right (res, rem) -> print res
+            Left e -> print e 
